c772b08318b77c75a6e1be02fbecae384e320337
Ryan Boyer <boyer.ryan@gmail.com>
2013-08-12 19:08:10 -0700

    chapter 1 of function programming in scala

---
+++ src/main/scala/arb/Chap1.scala
@@ -0,0 +1,16 @@
+package arb
+
+object Chapter1 {
+
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    (1 until as.length).forall(i => gt(as(i), as(i - 1)))
+  }
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = f(a, _)
+
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) =  (a:A) => (b: B) => f(a,b)
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a:A, b:B) => f(a)(b)
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = (a:A)=>f(g(a))
+}
---
+++ src/test/scala/arb/Chap1Suite.scala
@@ -0,0 +1,34 @@
+package arb
+
+import org.scalatest.FunSuite
+import arb.Chapter1._
+
+class Chap1Suite extends FunSuite {
+  val gt = (x: Int, y: Int) => x > y
+
+  test("is sorted with sorted elements") {
+    assert(isSorted(Array(1, 2, 3, 4), gt))
+  }
+
+  test("is not sorted last element off") {
+    assert(!isSorted(Array(1, 2, 3, 2), gt))
+  }
+
+  test("is not sorted first element off") {
+    assert(!isSorted(Array(5, 2, 3, 7), gt))
+  }
+
+  test("sorted empty list") {
+    assert(isSorted(Array[Int](), gt))
+  }
+
+  test("partial func") {
+    assert(partial1(3, (a:Int,b:Int)=> a < b)(4))
+    assert(partial1("dog", (a:String,b:String)=> (a+b).length)("cat") == 6)
+  }
+
+  test("curry") {
+    assert(curry((a:Int, b:String)=> b.length > a)(2)("dog"))
+  }
+
+}
